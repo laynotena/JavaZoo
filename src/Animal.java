a1bf4ec42cb0579a1fb0c11e1c6f7e0adbdbdf88
laynotena <49330818+laynotena@users.noreply.github.com>
2021-10-06 16:29:54 +0800

    Update Animal.java

--- src/Animal.java
+++ src/Animal.java
@@ -1,4 +1,7 @@
 public class Animal {
+    
+    public Animal(){
+    private int count;
     public double height;
     public double weight;
     public int age;
@@ -9,18 +12,23 @@ public class Animal {
     public int MP;
     public int upper_MP;
     public int LV = 0;
+    count++;
+    }
+    
     public void eat(){
         this.MP+=100;
         if (this.MP>=this.upper_MP){
             this.MP=this.upper_MP;
         }
     }
+    
     public void sleep(){
         this.MP-=10;
         if (this.MP<=0){
             this.MP=0;
         }
     }
+    
     public void move(){
         this.MP-=2;
         if (this.MP<=0){
@@ -28,6 +36,7 @@ public class Animal {
             this.eat();
         }
     }
+    
     public void get_value(){
         System.out.println(this.type + " : " + this.name +" "+ this.gender+" 身高："+this.height + " (m) "+" 體重："+
                 this.weight + "(kg) " + this.age + "歲 " +" HP:" +this.HP+ "   MP:"+this.MP+"  LV."+this.LV);
